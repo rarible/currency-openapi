d0d5f82bb2dcf2aa64ed513572d48b12306daeec
Artem Yakischik <a.yakischik@rarible.com>
2022-10-25 11:49:30 +0400

    feat: Swarm url provider removed

--- protocol-client-currency-starter/src/main/kotlin/com/rarible/protocol/currency/api/client/autoconfigure/CurrencyApiClientAutoConfiguration.kt
+++ protocol-client-currency-starter/src/main/kotlin/com/rarible/protocol/currency/api/client/autoconfigure/CurrencyApiClientAutoConfiguration.kt
@@ -1,13 +1,11 @@
 package com.rarible.protocol.currency.api.client.autoconfigure
 
-import com.rarible.core.application.ApplicationEnvironmentInfo
 import com.rarible.protocol.currency.api.client.CompositeWebClientCustomizer
 import com.rarible.protocol.currency.api.client.CurrencyApiClientFactory
 import com.rarible.protocol.currency.api.client.CurrencyApiServiceUriProvider
 import com.rarible.protocol.currency.api.client.DefaultCurrencyWebClientCustomizer
 import com.rarible.protocol.currency.api.client.K8sCurrencyApiServiceUriProvider
 import com.rarible.protocol.currency.api.client.NoopWebClientCustomizer
-import com.rarible.protocol.currency.api.client.SwarmCurrencyApiServiceUriProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
@@ -17,9 +15,7 @@ import org.springframework.context.annotation.Bean
 
 const val CURRENCY_WEB_CLIENT_CUSTOMIZER = "CURRENCY_WEB_CLIENT_CUSTOMIZER"
 
-class CurrencyApiClientAutoConfiguration(
-    private val applicationEnvironmentInfo: ApplicationEnvironmentInfo
-) {
+class CurrencyApiClientAutoConfiguration {
 
     @Autowired(required = false)
     @Qualifier(CURRENCY_WEB_CLIENT_CUSTOMIZER)
@@ -27,13 +23,8 @@ class CurrencyApiClientAutoConfiguration(
 
     @Bean
     @ConditionalOnMissingBean(CurrencyApiServiceUriProvider::class)
-    fun currencyApiServiceUriProvider(
-        @Value("\${rarible.core.client.k8s:false}") k8s: Boolean
-    ): CurrencyApiServiceUriProvider {
-        return if (k8s)
-            K8sCurrencyApiServiceUriProvider(applicationEnvironmentInfo.name)
-        else
-            SwarmCurrencyApiServiceUriProvider(applicationEnvironmentInfo.name)
+    fun currencyApiServiceUriProvider(): CurrencyApiServiceUriProvider {
+        return K8sCurrencyApiServiceUriProvider()
     }
 
     @Bean
