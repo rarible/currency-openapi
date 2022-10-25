a879cc9b87517e9d79db9189b62d1709ed2b2e67
Artem Yakischik <a.yakischik@rarible.com>
2022-10-25 12:06:41 +0400

    Revert "feat: Swarm url provider removed"
    This reverts commit d0d5f82bb2dcf2aa64ed513572d48b12306daeec.

--- protocol-client-currency-starter/src/main/kotlin/com/rarible/protocol/currency/api/client/autoconfigure/CurrencyApiClientAutoConfiguration.kt
+++ protocol-client-currency-starter/src/main/kotlin/com/rarible/protocol/currency/api/client/autoconfigure/CurrencyApiClientAutoConfiguration.kt
@@ -1,11 +1,13 @@
 package com.rarible.protocol.currency.api.client.autoconfigure
 
+import com.rarible.core.application.ApplicationEnvironmentInfo
 import com.rarible.protocol.currency.api.client.CompositeWebClientCustomizer
 import com.rarible.protocol.currency.api.client.CurrencyApiClientFactory
 import com.rarible.protocol.currency.api.client.CurrencyApiServiceUriProvider
 import com.rarible.protocol.currency.api.client.DefaultCurrencyWebClientCustomizer
 import com.rarible.protocol.currency.api.client.K8sCurrencyApiServiceUriProvider
 import com.rarible.protocol.currency.api.client.NoopWebClientCustomizer
+import com.rarible.protocol.currency.api.client.SwarmCurrencyApiServiceUriProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
@@ -15,7 +17,9 @@ import org.springframework.context.annotation.Bean
 
 const val CURRENCY_WEB_CLIENT_CUSTOMIZER = "CURRENCY_WEB_CLIENT_CUSTOMIZER"
 
-class CurrencyApiClientAutoConfiguration {
+class CurrencyApiClientAutoConfiguration(
+    private val applicationEnvironmentInfo: ApplicationEnvironmentInfo
+) {
 
     @Autowired(required = false)
     @Qualifier(CURRENCY_WEB_CLIENT_CUSTOMIZER)
@@ -23,8 +27,13 @@ class CurrencyApiClientAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(CurrencyApiServiceUriProvider::class)
-    fun currencyApiServiceUriProvider(): CurrencyApiServiceUriProvider {
-        return K8sCurrencyApiServiceUriProvider()
+    fun currencyApiServiceUriProvider(
+        @Value("\${rarible.core.client.k8s:false}") k8s: Boolean
+    ): CurrencyApiServiceUriProvider {
+        return if (k8s)
+            K8sCurrencyApiServiceUriProvider(applicationEnvironmentInfo.name)
+        else
+            SwarmCurrencyApiServiceUriProvider(applicationEnvironmentInfo.name)
     }
 
     @Bean
--- protocol-client-currency/src/main/kotlin/com/rarible/protocol/currency/api/client/K8sCurrencyApiServiceUriProvider.kt
+++ protocol-client-currency/src/main/kotlin/com/rarible/protocol/currency/api/client/K8sCurrencyApiServiceUriProvider.kt
@@ -2,9 +2,11 @@ package com.rarible.protocol.currency.api.client
 
 import java.net.URI
 
-class K8sCurrencyApiServiceUriProvider() : CurrencyApiServiceUriProvider {
+class K8sCurrencyApiServiceUriProvider(
+    private val env: String
+) : CurrencyApiServiceUriProvider {
 
     override fun getUri(): URI {
-        return URI.create("http://protocol-currency-api:8080")
+        return URI.create("http://protocol-currency-api.${env}-protocol:8080")
     }
 }
